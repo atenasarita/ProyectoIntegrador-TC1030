3a84c2cf64a959596c8f1e6ba373359c574ebd93
Atenas Arita <150080590+atenasarita@users.noreply.github.com>
2024-10-16 19:35:48 -0600

    Create PersonalityQuiz.h

---
+++ PersonalityQuiz.h
@@ -0,0 +1,11 @@
+#ifndef PERSONALITY_QUIZ_H
+#define PERSONALITY_QUIZ_H
+using namespace std;
+#include <iostream>
+
+
+int personalityQuiz();
+int calcularResultado(int respuestas[]);
+string obtenerDescripcion(int resultado);
+
+#endif // PERSONALITY_QUIZ_H
